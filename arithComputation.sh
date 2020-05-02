97c24fd5881b80ee7aed6a91a602a54528bc53f7
anubhav230 <anubhavsingh230@gmail.com>
2020-05-02 11:19:41 +0530

    number input has been taken

---
+++ arithComputation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+read -p "Emter first number::" number1
+read -p "Enter second number::" number2
+read -p "Enter third number::" number3
+
+echo "$number1"
+echo "$number2"
+echo "$number3"
