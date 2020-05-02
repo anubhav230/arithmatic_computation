f9f53bb67c9982a1711924dd84dda6512ebae13c
anubhav230 <anubhavsingh230@gmail.com>
2020-05-02 11:39:59 +0530

    second calculation has been done

--- arithComputation.sh
+++ arithComputation.sh
@@ -3,5 +3,8 @@ read -p "Emter first number::" number1
 read -p "Enter second number::" number2
 read -p "Enter third number::" number3
 
-answer=$(( number1+number2*number3))
-echo "$answer"
+answer1=$(( number1+number2*number3 ))
+echo "$answer1"
+
+answer2=$(( number1*number2+number3 ))
+echo "$answer2"
