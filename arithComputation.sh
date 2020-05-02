f61fe68812151772287315f67303e739fae73885
anubhav230 <anubhavsingh230@gmail.com>
2020-05-02 12:14:33 +0530

    result have added to dictionary

--- arithComputation.sh
+++ arithComputation.sh
@@ -5,12 +5,22 @@ read -p "Enter third number::" number3
 
 answer1=$(( number1+number2*number3 ))
 echo "$answer1"
+dictionary[1]=$answer1
 
 answer2=$(( number1*number2+number3 ))
 echo "$answer2"
+dictionary[2]=$answer2
 
 answer3=$(( number2+number1/number2 ))
 echo "$answer3"
+dictionary[3]=$answer3
 
 answer4=$(echo "scale=2; $number1 / $number2 + number3"|bc)
 echo "$answer4"
+dictionary[4]=$answer4
+
+
+
+echo ${dictionary[@]}
+
+echo ${!dictionary[@]}
